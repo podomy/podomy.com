eab7cd3decb2de7a4dbe208279c3551a3f24367c
neiltalap <contact@neiltalap.com>
2026-08-15 18:20:57 +0500

    Configure trailingSlash ignore and normalize active link path matching

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+	trailingSlash: 'ignore',
 	markdown: {
 		shikiConfig: {
 			theme: 'github-light',
